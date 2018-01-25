7d6c9e6ebecc4d40119bc51939709d082a5adb29
Erica Viana <erica.viana01@gmail.com>
2018-01-25 21:11:10 -0200

    Implementando ArrayList listaProdutos e incluindo código para adicionar produtos

--- src/main/java/com/produto/CadastraProduto.java
+++ src/main/java/com/produto/CadastraProduto.java
@@ -3,9 +3,11 @@ package com.produto;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class CadastraProduto {
+    private ArrayList<Produto> listaProdutos = new ArrayList<Produto>();
     private static final DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
 
     public String cadastrar(String codigo, String nome, double preco, int quantidadeEstoque, int estoqueMinima, Date dataCadastro) throws ParseException {
@@ -13,6 +15,7 @@ public class CadastraProduto {
         if(codigo == null || validaDataCadastro(dataCadastro)) {
             return "Produto nao cadastrado";
         }
+        listaProdutos.add(produto);
         return "Cadastrada com sucesso!!";
     }
 
