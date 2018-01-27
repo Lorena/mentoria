1b4f9a3323055cdbb5c613a9c419741a4b0db75a
Lorena Souza <lcsouza@thoughtworks.com>
2018-01-27 15:52:01 -0200

    Criei execicíos para a Erica fazer, importante que ela crie os testes e os métodos para tudo ficar verde

---
+++ src/main/java/com/produto/Produtos.java
@@ -0,0 +1,39 @@
+package com.produto;
+
+import com.produto.model.Produto;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class Produtos {
+
+    private List<Produto> produtos = new ArrayList<Produto>();
+
+    public List<Produto> cadastraProdutos(int codigo, String nome, double preco, int quantidadeEstoque, int estoqueMinima, Date dataCadastro) throws Exception {
+        CadastraProduto cadastraProduto = new CadastraProduto();
+
+        //Erica implementar o método para o teste passar
+
+        return produtos;
+    }
+
+    public List<Produto> getProdutosTemNoEstoque() {
+
+        List<Produto> produtoQueTemNoEstoque = new ArrayList<Produto>();
+
+        for(Produto produto : produtos) {
+            if(produto.getQuantidadeEstoque() != 0) {
+                produtoQueTemNoEstoque.add(produto);
+            }
+        }
+        return produtoQueTemNoEstoque;
+    }
+
+    public List<Produto> getProdutosPorNome(String nome) {
+
+        //Erica Implementar o método para o teste passar
+
+        return null;
+    }
+}
---
+++ src/test/java/com/produto/ProdutosTest.java
@@ -0,0 +1,74 @@
+package com.produto;
+
+import com.produto.model.Produto;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class ProdutosTest {
+
+    private Produtos produtos;
+
+    @Before
+    public void setUp() throws Exception {
+        produtos = new Produtos();
+    }
+
+    @Test
+    public void deveCadastrarMultiplosProdutos() throws Exception {
+
+        Date dataCadastro = new SimpleDateFormat("yyyyMMdd").parse("20180118");
+
+        List<Produto> produtoList = produtos.cadastraProdutos(1, "Nome1", 0, 1, 2, dataCadastro);
+        assertThat(produtoList.size(), is(1));
+
+        produtoList = produtos.cadastraProdutos(2, "Nome2", 0, 1, 2, dataCadastro);
+        assertThat(produtoList.size(), is(2));
+    }
+
+    @Test
+    public void devePegarTodosOsProdutosCadastradosQueTemNoEstoque() throws Exception {
+
+        Date dataCadastro = new SimpleDateFormat("yyyyMMdd").parse("20180118");
+        produtos.cadastraProdutos(1, "Nome1", 100, 1, 2, dataCadastro);
+        produtos.cadastraProdutos(2, "Nome2", 101, 0, 2, dataCadastro);
+
+        List<Produto> produtosTemNoEstoque = produtos.getProdutosTemNoEstoque();
+
+        assertThat(produtosTemNoEstoque.size(), is(1));
+    }
+
+
+    @Test
+    public void devePegarProdutoPeloNome() throws Exception {
+
+        Date dataCadastro = new SimpleDateFormat("yyyyMMdd").parse("20180118");
+        produtos.cadastraProdutos(1, "Nome1", 100, 1, 2, dataCadastro);
+        produtos.cadastraProdutos(2, "Nome2", 101, 0, 2, dataCadastro);
+
+        List<Produto> produtosPorNome = produtos.getProdutosPorNome("Nome2");
+
+        assertThat(produtosPorNome.size(), is(1));
+        assertThat(produtosPorNome.get(0).getNome(), is("Nome2"));
+
+    }
+
+
+    @Test
+    public void devePegarProdutoCadastradoNaDataInformada() throws Exception {
+        //Erica fazer o teste e implementar o método para fazer o teste passar.
+
+    }
+
+    @Test
+    public void devePegarProdutoComPrecoMaiorDoQuePrecoInformado() throws Exception {
+        //Erica fazer o teste e implementar o método para fazer o teste passar.
+
+    }
+}
