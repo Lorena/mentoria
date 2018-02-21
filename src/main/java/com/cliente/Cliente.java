8770cfffd86fb27bd8e401b567ce41b059dc0166
Erica Viana <erica.viana01@gmail.com>
2018-02-21 16:41:36 -0300

    Criando o teste naoDeveCadastrarClienteQuandoNaoExistirCpf, na classe ClienteTest

--- src/main/java/com/cliente/Cliente.java
+++ src/main/java/com/cliente/Cliente.java
@@ -1,6 +1,10 @@
 package com.cliente;
 
+import java.util.Date;
+
 public class Cliente {
+    public Cliente(int codigo, String cpf, String nome, String telefone, String email, String endereco, Date dataCadastro) {
+    }
 
     //Não construir a classe cliente gerando todos os atributos
     //Antes de colocar qualquer coisa na classe cliente, primeiro fazer testes para ela.
--- src/test/java/com/cliente/ClienteTest.java
+++ src/test/java/com/cliente/ClienteTest.java
@@ -1,7 +1,16 @@
 package com.cliente;
 
+import org.junit.Test;
+import com.cliente.Cliente;
+
 public class ClienteTest {
 
     //pode criar os testes com qualquer ideia que vc tiver....
+
+    @Test(expected = Exception.class)
+    public void naoDeveCadastrarClienteQuandoNaoExistirCpf() throws Exception {
+     new Cliente(1, null, "Erica", "98999-9999", "erica@email.com", "Rua ABC, 123", null);
+    }
+
 
 }
