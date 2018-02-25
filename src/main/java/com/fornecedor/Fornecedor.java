40b297d25bbbadb16408890f7bdb16e7c96aeb0b
Erica Viana <erica.viana01@gmail.com>
2018-02-25 17:46:50 -0300

    Construindo o método Fornecedor, da classe Fornecedor, para atender o teste naoDeveCadastrarFornecedorQuandoNaoExistirCnpj

--- src/main/java/com/fornecedor/Fornecedor.java
+++ src/main/java/com/fornecedor/Fornecedor.java
@@ -4,7 +4,22 @@ import java.util.Date;
 
 public class Fornecedor {
 
-    public Fornecedor(int codigo, String cnpj, String nome, String telefone, String email, Date dataCadastro) {
+    private int codigo;
+    private String cnpj;
+    private String nome;
+    private String telefone;
+    private String email;
+    private Date dataCadastro;
 
+    public Fornecedor(int codigo, String cnpj, String nome, String telefone, String email, Date dataCadastro) throws Exception{
+        if(cnpj == null || cnpj == " "){
+            throw new Exception("Fornecedor não cadastrado");
+        }
+        this.codigo = codigo;
+        this.cnpj = cnpj;
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.dataCadastro = dataCadastro;
     }
 }
