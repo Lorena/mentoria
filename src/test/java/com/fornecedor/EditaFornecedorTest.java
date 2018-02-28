5f161454c9ae850382aa15685e36ea3f64af687f
Erica Viana <erica.viana01@gmail.com>
2018-02-28 16:15:27 -0300

    Construindo o teste deveEditarNomeDoFornecedorERetornarMensagemDeSucesso

---
+++ src/test/java/com/fornecedor/EditaFornecedorTest.java
@@ -0,0 +1,23 @@
+package com.fornecedor;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class EditaFornecedorTest {
+
+    @Test
+    public void deveEditarNomeDoFornecedorERetornarMensagemDeSucesso() throws Exception {
+        Date data = new Date();
+        Fornecedor fornecedor = new Fornecedor(1, "12.345.678/0000-01", "Forn1", "98888-9999", "forn1@email.com", data);
+
+        EditaFornecedor editaFornecedor = new EditaFornecedor();
+        String mensagem = editaFornecedor.editaNomeFornecedor("Fornecedor Um");
+
+        assertThat(mensagem, is("Fornecedor alterado com sucesso."));
+    }
+
+}
