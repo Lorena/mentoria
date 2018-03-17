819e7c06e2b96a2dc1bfa3265d31d9bd372cd78b
Erica Viana <erica.viana01@gmail.com>
2018-03-17 14:35:10 -0300

    Construindo o método verificaDataPedidoAntesDeQuintaFeira

--- src/main/java/com/whiteGas/WhiteGas.java
+++ src/main/java/com/whiteGas/WhiteGas.java
@@ -1,19 +1,33 @@
 package com.whiteGas;
 
 import com.pedido.Pedido;
-
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.GregorianCalendar;
 
 public class WhiteGas {
     private Pedido pedido;
     private int qtdNovosCilindros;
     private Date dataPedido;
 
-    public WhiteGas(Pedido pedido, int qtdNovosCilindros, Date dataPedido) {
+
+    public WhiteGas(Pedido pedido, int qtdNovosCilindros, Date dataPedido) throws Exception {
+        verificaDataPedidoAntesDeQuintaFeira(dataPedido);
         this.pedido = pedido;
         this.qtdNovosCilindros = qtdNovosCilindros;
         this.dataPedido = dataPedido;
     }
 
+    private void verificaDataPedidoAntesDeQuintaFeira(Date dataPedido) throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        Date dataValidar = sdf.parse(String.valueOf(dataPedido));
+        GregorianCalendar gc = new GregorianCalendar();
+        gc.setTime(dataValidar);
+        int diaDaSemana = gc.get(GregorianCalendar.DAY_OF_WEEK);
+
+        if(diaDaSemana >= 5){
+            throw new Exception("Os pedidos são fechados às quintas-feiras.");
+        }
+    }
 
 }
