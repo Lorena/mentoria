7c08fb468389bf32374d5dc6a2b0d0ad60965da9
Erica Viana <erica.viana01@gmail.com>
2018-03-17 11:31:08 -0300

    Ajustando o construtor da classe Xyz, para atender o teste deveRetornarErroSeNaoHouverDezCilindrosCheios

--- src/main/java/com/xyz/Xyz.java
+++ src/main/java/com/xyz/Xyz.java
@@ -7,7 +7,10 @@ public class Xyz {
     private int qtdCilindroCheio;
     private Pedido pedido;
 
-    public Xyz(int qtdCilindroCheio, Pedido pedido) {
+    public Xyz(int qtdCilindroCheio, Pedido pedido) throws Exception {
+        if(qtdCilindroCheio < 10){
+            throw new Exception("O estoque de cilindros cheios não pode ser menor que 10.");
+        }
         this.qtdCilindroCheio = qtdCilindroCheio;
         this.pedido = pedido;
     }
