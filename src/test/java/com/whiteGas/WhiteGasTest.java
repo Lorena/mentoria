eeab85c1ed90f6228578971f01c254f22d3627d1
Erica Viana <erica.viana01@gmail.com>
2018-03-17 14:10:30 -0300

    Construindo o teste deveRetornarErroSeDataPedidoForDepoisDeQuintaFeira

--- src/test/java/com/whiteGas/WhiteGasTest.java
+++ src/test/java/com/whiteGas/WhiteGasTest.java
@@ -2,10 +2,8 @@ package com.whiteGas;
 
 import com.pedido.Pedido;
 import org.junit.Test;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.nullValue;
@@ -16,7 +14,7 @@ public class WhiteGasTest {
     @Test
     public void deveCadastrarPedidoParaWhiteGasComSucesso() throws Exception {
         int qtdNovosCilindros = 1;
-        Date dataPedido = new Date();
+        Date dataPedido = new SimpleDateFormat("yyyyMMdd").parse("20180314");;
         Date dataEntrega = new SimpleDateFormat("yyyyMMdd").parse("20180330");
 
         Pedido pedido = new Pedido("Gás Hélio", 5.0, "Rua ABC", "Rua ABC", dataPedido, dataEntrega);
@@ -25,4 +23,14 @@ public class WhiteGasTest {
         assertThat(whiteGas, is(not(nullValue())));
     }
 
+    @Test(expected = Exception.class)
+    public void deveRetornarErroSeDataPedidoForDepoisDeQuintaFeira() throws Exception {
+        int qtdNovosCilindros = 1;
+        Date dataPedido = new Date();
+        Date dataEntrega = new SimpleDateFormat("yyyyMMdd").parse("20180330");
+
+        Pedido pedido = new Pedido("Gás Hélio", 5.0, "Rua ABC", "Rua ABC", dataPedido, dataEntrega);
+
+        WhiteGas whiteGas = new WhiteGas(pedido, qtdNovosCilindros, dataPedido);
+    }
 }
