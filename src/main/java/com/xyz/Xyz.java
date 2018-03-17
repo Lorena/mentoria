4fd5dd30f3fed1381e7d9976c5ec1de57ce917fd
Erica Viana <erica.viana01@gmail.com>
2018-03-17 12:36:28 -0300

    Criando o método validaEstoqueDeCilindro e refatorando a classe Xyz

--- src/main/java/com/xyz/Xyz.java
+++ src/main/java/com/xyz/Xyz.java
@@ -12,12 +12,20 @@ public class Xyz {
 
     public Xyz(int qtdCilindroGasHelio, int qtdCilindroMonoxido, int qtdCilindroDioxido, int qtdCilindroCheio, Pedido pedido)
             throws  Exception{
-        validaSePossuiDezCilindrosCheios(qtdCilindroCheio);
+        validaEstoqueDeCilindros(qtdCilindroCheio, qtdCilindroDioxido, qtdCilindroMonoxido, qtdCilindroGasHelio);
         this.qtdCilindroGasHelio = qtdCilindroGasHelio;
         this.qtdCilindroMonoxido = qtdCilindroMonoxido;
         this.qtdCilindroDioxido = qtdCilindroDioxido;
         this.qtdCilindroCheio = qtdCilindroCheio;
         this.pedido = pedido;
+    }
+
+    private void validaEstoqueDeCilindros(int qtdCilindroCheio, int qtdCilindroDioxido, int qtdCilindroMonoxido, int qtdCilindroGasHelio)
+    throws Exception{
+        validaSePossuiDezCilindrosCheios(qtdCilindroCheio);
+        validaSeExisteCilindroDeDioxidoDisponivel(qtdCilindroDioxido);
+        validaSeExisteCilindroDeMonoxidoDisponivel(qtdCilindroMonoxido);
+        validaSeExisteCilindroDeGasHelioDisponivel(qtdCilindroGasHelio);
     }
 
     private void validaSePossuiDezCilindrosCheios(int qtdCilindroCheio) throws Exception {
@@ -26,4 +34,21 @@ public class Xyz {
         }
     }
 
+    private void validaSeExisteCilindroDeGasHelioDisponivel(int qtdCilindroGasHelio) throws Exception {
+        if(qtdCilindroGasHelio == 0){
+            throw new Exception("Não possuímos cilindros de Gás Hélio disponível. Iremos solicitar para o fornecedor.");
+        }
+    }
+
+    private void validaSeExisteCilindroDeMonoxidoDisponivel(int qtdCilindroMonoxido) throws Exception {
+        if(qtdCilindroMonoxido == 0){
+            throw new Exception("Não possuímos cilindros de Monóxido de Carbono disponível. Iremos solicitar para o fornecedor.");
+        }
+    }
+
+    private void validaSeExisteCilindroDeDioxidoDisponivel(int qtdCilindroDioxido) throws Exception {
+        if(qtdCilindroDioxido == 0){
+            throw new Exception("Não possuímos cilindros de Dióxido de Carbono disponível. Iremos solicitar para o fornecedor.");
+        }
+    }
 }
